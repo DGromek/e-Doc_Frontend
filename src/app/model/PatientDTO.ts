ce2c867deecc8d2f91ffaf1addc5cd731788c992
DGromek <dawidgromek.1997@gmail.com>
2020-04-09 17:02:44 +0200

    Added PatientDTO

---
+++ src/app/model/PatientDTO.ts
@@ -0,0 +1,26 @@
+import {Patient} from './patient';
+
+export class PatientDTO {
+  public pesel: string;
+  public firstName: string;
+  public lastName: string;
+  public city: string;
+  public street: string;
+  public houseNr: string;
+  public postalCode: string;
+  public password: string;
+  public phoneNr: string;
+  public email: string;
+  constructor(patient: Patient) {
+    this.pesel = patient.pesel;
+    this.firstName = patient.firstName;
+    this.lastName = patient.lastName;
+    this.password = patient.password;
+    this.city = patient.city;
+    this.postalCode = patient.postalCode;
+    this.street = patient.street;
+    this.houseNr = patient.houseNr;
+    this.email = patient.email;
+    this.phoneNr = patient.phoneNr;
+  }
+}
