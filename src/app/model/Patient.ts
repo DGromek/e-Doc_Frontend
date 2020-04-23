0b2ef43f597ca92778489155a622b871e53e842a
DGromek <dawidgromek.1997@gmail.com>
2020-04-23 16:38:00 +0200

    Refactored Patient class.

---
+++ src/app/model/Patient.ts
@@ -0,0 +1,15 @@
+export class Patient {
+  public pesel: string;
+  public firstName: string;
+  public lastName: string;
+  public city: string;
+  public street: string;
+  public houseNr: string;
+  public postalCode: string;
+  public password: string;
+  public repeatedPassword: string;
+  public phoneNr: string;
+  public email: string;
+  public approval: boolean;
+  constructor() { }
+}
