f694bad9504b85047f896e07bb67364ca472fb09
DGromek <dawidgromek.1997@gmail.com>
2020-04-24 17:08:16 +0200

    Added logout

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -12,4 +12,11 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  isUserLogged() {
+    return localStorage.getItem('token') !== null;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
 }
